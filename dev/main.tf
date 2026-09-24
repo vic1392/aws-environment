098a09e2f6f10d941a372242b6a8310276306af8
vic1392 <vicky13921392@gmail.com>
2026-09-24 10:04:39 +0530

    Create main.tf

---
+++ dev/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets belonging to default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# S3 module
+module "s3" {
+  source = "../../modules/s3"
+
+  bucket_name = var.bucket_name
+  environment = "dev"
+}
+
+# EC2 module
+module "ec2" {
+  source = "../../modules/ec2"
+
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = data.aws_subnets.default.ids[0]
+
+  instance_name = "dev-ec2"
+
+  s3_bucket_arn = module.s3.bucket_arn
+}
